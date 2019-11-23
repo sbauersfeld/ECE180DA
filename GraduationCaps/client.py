c48af2c6121af18ed5f30c6ca795a26b614dc7f7
Scott <sbauersfeld@g.ucla.edu>
2019-11-22 16:05:55 -0800

    new client api

--- GraduationCaps/client.py
+++ GraduationCaps/client.py
@@ -1,5 +1,6 @@
 import sys
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt_client
+import paho.mqtt.subscribe as subscribe
 
 class Client:
     def __init__(self, my_id):
@@ -20,7 +21,7 @@ class Client:
         client.loop_stop()
 
     def get_info(self):
-        client = mqtt.Client(self.id)
+        client = mqtt_client.Client(self.id)
         client.connect("broker.hivemq.com")
         topic = "ee180d/group4/" + self.id
         print(topic)
@@ -31,11 +32,16 @@ class Client:
 
 def main():
     DEVICE = sys.argv[1]
-    my_client = Client(DEVICE)
-    my_client.get_info()
-    print("Num rows:", my_client.num_rows)
-    print("Num cols:", my_client.num_cols)
-    print("My coords:",my_client.coords)
+    # my_client = Client(DEVICE)
+    # my_client.get_info()
+    msg = subscribe.simple("ee180d/group4/" + DEVICE, hostname="broker.hivemq.com")
+    msg = msg.payload.decode().split()
+    num_rows = msg[0]
+    num_cols = msg[1]
+    coords = msg[2:]
+    print("Num rows:", num_rows)
+    print("Num cols:", num_cols)
+    print("My coords:", coords)
 
 if __name__ == '__main__':
     main()
