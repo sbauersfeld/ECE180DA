14aea9badd5a6ec239d691d1517edfef86c91bb4
Wilson <wlin9608@gmail.com>
2019-12-06 04:29:15 +0000

    Added new demo

--- GraduationCaps/LED_display/demos/test.py
+++ GraduationCaps/LED_display/demos/test.py
@@ -2,10 +2,11 @@
 from display_demo import *
 from input_pixelator import *
 
-data, data_width = input_pixelator(2, 2, "what the hell", "fonts/font.ttf")
+data, data_width = input_pixelator(4, 5, "a", "fonts/font.ttf")
 print(data)
 print(data_width)
-sequences = assign_seq(data_width, 2, 2, data)
+sequences = assign_seq(data_width, 5, 4, data)
 print(sequences)
-display([15, 14, 23, 18], sequences, fp=0.5)
+print(len(sequences))
+display([14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21, 26, 19, 13, 6, 5, 11, 2, 22], sequences)
 
